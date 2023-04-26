7a5a99099609c10a8b4b0d1bbf28de23a8a0824e
Jamie Churchouse <jamespeterchurchouse@gmail.com>
2023-04-27 11:19:32 +1200

    read an image from teh camera

--- Tutorial2/Tutorial2.py
+++ Tutorial2/Tutorial2.py
@@ -5,18 +5,24 @@ import cv2 as cv
 import numpy as np
 
 def main():
+    filepath = os.getcwd() + "/Tutorial2/frame.png"
 
-    filepath = os.getcwd() + "/Tutorial2/apples.png"
-    img = cv.imread(filepath, cv.IMREAD_COLOR)
+    camera = cv.VideoCapture(0)
 
-    if (img is None):
-        print(f"Error: {filepath} Image not found!!!")
+    if (not camera.isOpened()):
+        print(f"ERR: not found")
         return 1
+    
+    ret, frame = camera.read()
 
-    #window = cv.namedWindow("img")
-    cv.imshow("img", img)
+    if (frame is None):
+        print(f"ERR - no image")
+        return 2
+
+    cv.imwrite(filepath, frame)
+    cv.imshow("frame", frame)
     cv.waitKey(0)
-    cv.destroyWindow("img")
+    cv.destroyWindow("frame")
     return 0
 
 if __name__ == "__main__":
